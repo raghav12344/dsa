22738a0966ff475fd91891c22b618ac803e93464
Raghav Gupta <graghav1233@gmail.com>
2026-08-05 19:51:17 +0530

    Time: 3 ms (73.14%) | Memory: 104 MB (95.09%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -1,40 +1,41 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    struct Info{
+    struct Info {
         bool isBST;
         int sum;
         int mn;
         int mx;
     };
-    Info dfs(TreeNode * root,int &ans)
-    {
-        if(root==NULL)
-            return {true,0,INT_MAX,INT_MIN};
-        
-        Info l=dfs(root->left,ans);
-        Info r=dfs(root->right,ans);
 
-        if(l.isBST && r.isBST && root->val>l.mx && root->val<r.mn )
-        {
-            ans=max(ans,l.sum+r.sum+root->val);
-            return {true,l.sum+r.sum+root->val,min(l.mn,root->val),max(r.mx,root->val)};
+    int ans = 0;
+
+    Info dfs(TreeNode* root) {
+        if (!root)
+            return {true, 0, INT_MAX, INT_MIN};
+
+        Info L = dfs(root->left);
+        Info R = dfs(root->right);
+
+        if (L.isBST && R.isBST &&
+            root->val > L.mx &&
+            root->val < R.mn) {
+
+            int currSum = L.sum + R.sum + root->val;
+            ans = max(ans, currSum);
+
+            return {
+                true,
+                currSum,
+                min(root->val, L.mn),
+                max(root->val, R.mx)
+            };
         }
-        return {false,0,INT_MIN,INT_MAX};
+
+        return {false, 0, INT_MIN, INT_MAX};
     }
+
     int maxSumBST(TreeNode* root) {
-        int ans=0;
-        dfs(root,ans);
+        dfs(root);
         return ans;
     }
 };
