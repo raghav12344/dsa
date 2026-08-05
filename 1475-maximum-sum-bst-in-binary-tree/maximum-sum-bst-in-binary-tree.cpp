af254be0ab577242d93c1c539cae85a43b251abd
Raghav Gupta <graghav1233@gmail.com>
2026-08-05 19:53:34 +0530

    Time: 4 ms (64.52%) | Memory: 104.3 MB (75.88%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -1,40 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    struct Info {
+    struct Info{
         bool isBST;
         int sum;
         int mn;
         int mx;
     };
+    int ans=0;
+    Info dfs(TreeNode * root)
+    {
+        if(root==NULL)
+            return {true,0,INT_MAX,INT_MIN};
+        
+        Info l=dfs(root->left);
+        Info r=dfs(root->right);
 
-    int ans = 0;
-
-    Info dfs(TreeNode* root) {
-        if (!root)
-            return {true, 0, INT_MAX, INT_MIN};
-
-        Info L = dfs(root->left);
-        Info R = dfs(root->right);
-
-        if (L.isBST && R.isBST &&
-            root->val > L.mx &&
-            root->val < R.mn) {
-
-            int currSum = L.sum + R.sum + root->val;
-            ans = max(ans, currSum);
-
-            return {
-                true,
-                currSum,
-                min(root->val, L.mn),
-                max(root->val, R.mx)
-            };
+        if(l.isBST && r.isBST && root->val>l.mx && root->val<r.mn )
+        {
+            ans=max(ans,l.sum+r.sum+root->val);
+            return {true,l.sum+r.sum+root->val,min(l.mn,root->val),max(r.mx,root->val)};
         }
-
-        return {false, 0, INT_MIN, INT_MAX};
+        return {false,0,INT_MIN,INT_MAX};
     }
-
     int maxSumBST(TreeNode* root) {
+        ans=0;
         dfs(root);
         return ans;
     }
