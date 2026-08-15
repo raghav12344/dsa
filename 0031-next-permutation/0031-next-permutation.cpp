a94be7f5f15961573125a2bc8e3fe6fb1b9eb540
Raghav Gupta <graghav1233@gmail.com>
2026-08-16 03:43:44 +0530

    Time: 0 ms (100%), Space: 15.8 MB (14.06%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+        int n=arr.size();
+        int pivot=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(arr[i+1]>arr[i])
+            {
+                pivot=i;
+                break;
+            }
+        }
+        if(pivot==-1)
+        {
+            reverse(arr.begin(),arr.end());
+            return ;
+        }
+        for(int i=n-1;i>pivot;i--)
+        {
+            if(arr[i]>arr[pivot])
+            {
+                swap(arr[i],arr[pivot]);
+                break;
+            }
+        }
+        reverse(arr.begin()+pivot+1,arr.end());
+    }
+};
