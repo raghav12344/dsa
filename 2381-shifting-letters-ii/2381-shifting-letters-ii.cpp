d99274deb375a350af12e8d0048bad5c85ae8dad
Raghav Gupta <graghav1233@gmail.com>
2026-08-16 22:08:36 +0530

    Time: 0 ms (100%), Space: 97.7 MB (89.19%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        
+        vector<int> diff(s.size()+1,0);
+
+        for(int i=0;i<shifts.size();i++)
+        {
+            int start=shifts[i][0];
+            int end=shifts[i][1];
+            int val=(shifts[i][2]==1)?1:-1;
+
+            diff[start]+=val;
+            diff[end+1]-=val;
+        }
+
+        int curr_shift=0;
+        for(int i=0;i<s.size();i++)
+        {
+            curr_shift+=diff[i];
+
+            int net_shift=((curr_shift%26)+26)%26;
+
+            s[i]='a'+(s[i]+net_shift-'a')%26;
+        }
+        return s;
     }
 };
