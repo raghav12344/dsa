b1b2077759b9ea08da85edd3bfc684e89ecd5a48
Raghav Gupta <graghav1233@gmail.com>
2026-08-23 02:42:09 +0530

    Time: 327 ms (6.83%), Space: 81.3 MB (12.62%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+// bool chk(string &str, int l, int r)
+// {
+//     if (l == r)
+//         return true;
+    
+//     vector<int> hash(256, 0); // initialize with 0 instead of -1
+//     for (int i = l; i <= r; i++)
+//     {
+//         hash[str[i]]++;
+//         if (hash[str[i]] > 1)
+//             return false;
+//     }
+//     return true;
+// }
+
+// int lengthOfLongestSubstring(string s)
+// {
+//     int mx = 0; // initialize mx = 0, not INT_MIN
+//     int l = 0, r = 0;
+//     while (r < s.length())
+//     {
+//         if (chk(s, l, r))
+//         {
+//             mx = max(mx, r - l + 1);
+//             r++; // only move r when current substring is valid
+//         }
+//         else
+//         {
+//             l++; // move left to shrink window
+//         }
+//     }
+//     return mx;
+// }
+
+
+
+int lengthOfLongestSubstring(string s)
+{
+    int mx = 0;
+    int l = 0, r = 0;
+    unordered_set<char> st;
+    
+    while (r < s.length())
+    {
+        if (st.find(s[r]) == st.end())
+        {
+            // Character not in set, insert it
+            st.insert(s[r]);
+            mx = max(mx, r - l + 1);
+            r++;
+        }
+        else
+        {
+            // Character already exists, remove from left
+            st.erase(s[l]);
+            l++;
+        }
+    }
+    
+    return mx;
+}
+
+
+};
