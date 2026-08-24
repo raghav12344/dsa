10070248c2da28eaa76d8698998ad60841432ab8
Raghav Gupta <graghav1233@gmail.com>
2026-08-24 05:32:13 +0530

    Time: 12 ms (93.09%), Space: 50.6 MB (29.26%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,63 @@
+class Trie {
+public:
+    bool isLeaf;
+    Trie *children[26];
+    Trie() {
+        isLeaf=false;
+        for(int i=0;i<26;i++)
+            children[i]=NULL;
+    }
+    
+    void insert(string word) {
+        Trie *curr=this;
+
+        for(char ch:word)
+        {
+            int idx=ch-'a';
+
+            if(curr->children[idx]==NULL)
+                curr->children[idx]=new Trie();
+            
+            curr=curr->children[idx];
+        }
+        curr->isLeaf=true;
+    }
+    
+    bool search(string word) {
+        Trie *curr=this;
+
+        for(char ch:word)
+        {
+            int idx=ch-'a';
+
+            if(curr->children[idx]==NULL)
+                return false;
+            
+            curr=curr->children[idx];
+        }
+        return curr->isLeaf;
+    }
+    
+    bool startsWith(string prefix) {
+        Trie * curr=this;
+
+        for(char ch:prefix)
+        {
+            int idx=ch-'a';
+
+            if(curr->children[idx]==NULL)
+                return false;
+            
+            curr=curr->children[idx];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
