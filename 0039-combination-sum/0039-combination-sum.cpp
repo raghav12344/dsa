a9dbd59171df5cca4cecd1f0c5836407f03c08b7
Raghav Gupta <graghav1233@gmail.com>
2026-08-28 23:40:03 +0530

    Time: 99 ms (5.03%), Space: 72.3 MB (5.7%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void recur(vector<int>& candidates, int target,vector<vector<int>> &res,vector<int>arr,int i,int sum)
+    void recur(vector<int>& candidates, int target,vector<vector<int>> &res,vector<int>arr,int i)
     {
-        if(i>=candidates.size())
-            return;
-        if(sum>target)
-            return ;
-        if(sum==target)
+        if(0==target)
         {
             res.push_back(arr);
             return;
         }
+        if(i>=candidates.size() || target<0)
+            return;
 
-        recur(candidates,target,res,arr,i+1,sum); //not chosing i;
         arr.push_back(candidates[i]);
-        recur(candidates,target,res,arr,i,sum+candidates[i]);  // chosing i again;
+        recur(candidates,target-candidates[i],res,arr,i); //not chosing i;
+        arr.pop_back();
+        recur(candidates,target,res,arr,i+1);  // chosing i again;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> arr;
         vector<vector<int>> res;
-        recur(candidates,target,res,arr,0,0);
+        recur(candidates,target,res,arr,0);
         return res;
     }
 };
